05fc7578081bdf745cf1eee0e3b2b9d29f8de962
daegun <daekun0920>
2018-03-28 14:58:09 +0900

    merge

---
+++ Client/ClientTest/src/Normal.java
@@ -0,0 +1,32 @@
+
+public class Normal extends Abs implements Inter {
+	
+
+	@Override
+	public void end() {  // 인터페이스를 구현
+		
+	}
+	
+	
+
+
+
+
+	@Override
+	public void start() { // 인터페이스를 구현
+	
+		
+	}
+
+
+
+
+
+
+
+	void blabla() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
